f59aed47d0218e0c960a9547cb3b9e1a9f776b1f
DeepakPrakasam <deepakp.22cse@kongu.edu>
2025-04-24 22:24:08 +0530

    vite config file  updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '/api': {
-          target: 'http://backend-container:4000',
+          target: env.VITE_API_BASE_URL,
           changeOrigin: true,
           secure: false,
         }
