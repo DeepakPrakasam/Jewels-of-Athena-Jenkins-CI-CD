fa17de217ed9099075ae4ed9c24548189cca4358
DeepakPrakasam <deepakp.22cse@kongu.edu>
2025-05-22 12:55:40 +0530

    Updated

--- frontend/src/components/PriceTicker.jsx
+++ frontend/src/components/PriceTicker.jsx
@@ -2,7 +2,7 @@ import Marquee from "react-fast-marquee";
 
 function PriceTicker() {
   return (
-    <div className="alert alert-info rounded-2">
+    <div className="alert alert-success rounded-2">
     <marquee behavior="alternate" direction="left">
     Today: <strong>Date 12-05-25 Gold</strong> 22k - 1g = Rs. 8770 | Today: <strong>Silver</strong> 1g = Rs. 110.00 | Today: <strong>Platinum</strong> 1g = Rs. 3227
     </marquee>
